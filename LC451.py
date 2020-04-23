b28e9af57b958a81bf6a0ee54a8bf251f9eb4b96
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-04-23 14:54:07 +0600

    Create LC451.py

---
+++ LC451.py
@@ -0,0 +1,14 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        
+        ls=list(s) #converting the string to list
+        dic=collections.Counter(ls)
+        
+        t=dic.most_common() # sorting the counter by value, This method returns a tuple
+        res=''
+        for (k,v) in t: # iterate through the tuple
+            for i in range(v):  
+                res+=k  # add characters to string res by frequency
+        return res
+        
+        
