9902321265e69e73fd19abb5588c17e41abe9c9e
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-04-24 15:21:47 +0600

    Create LC150.py

---
+++ LC150.py
@@ -0,0 +1,23 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        operator=set(['+','-','*','/'])
+        
+        stk=[]
+        for i in tokens:
+            if i not in operator:
+                stk.append(i)
+            else:
+                a=int(stk.pop())
+                b=int(stk.pop())
+                if i=='+':
+                    stk.append(a+b)
+                elif i=='-':
+                    stk.append(b-a)
+                elif i=='*':
+                    stk.append(a*b)
+                elif i=='/':
+                    stk.append(b/a)
+        return int(stk[-1])
+                
+        
