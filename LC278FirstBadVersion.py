e4f0932db78ec8d0774611c8a0266aaf2d27c58e
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-05-01 22:02:51 +0600

    Create LC278FirstBadVersion.py

---
+++ LC278FirstBadVersion.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left=1
+        right=n
+        
+        while left<right:
+            mid=(left+right)//2
+            
+            if not isBadVersion(mid):
+                left=mid+1
+            else:
+                right=mid
+        return left
+                
+                
