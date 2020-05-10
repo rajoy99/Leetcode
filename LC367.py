396e6cfefedfa84e66915c4f6364cb0e599e5703
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-05-10 14:39:19 +0600

    Create LC367.py

---
+++ LC367.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        
+        lo=1
+        hi=num
+        
+        while lo<=hi:
+            mid=(lo+hi)//2
+            if mid*mid==num:
+                return True
+            elif mid*mid>num:
+                hi=mid-1
+            elif mid*mid<num:
+                lo=mid+1
+        return False
+        
