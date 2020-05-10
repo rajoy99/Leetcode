4da88e79e5dbb7c79e21ef29d3a1415b3b417f13
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-05-10 14:40:05 +0600

    Create RotateImage.py

---
+++ RotateImage.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for row in range(len(matrix)):
+            for col in range(row,len(matrix[0])):
+                matrix[row][col],matrix[col][row]=matrix[col][row],matrix[row][col]
+                
+        
+        for i in matrix:
+            i.reverse()
+        
