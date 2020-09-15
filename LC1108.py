4ca6584f15039968ffaef20c41fc65fdca448a88
exponential-tortoise <61883509+exponential-tortoise@users.noreply.github.com>
2020-09-16 00:34:58 +0600

    Create LC1108.py

---
+++ LC1108.py
@@ -0,0 +1,14 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        
+        ans=''
+        
+        for i in address:
+            if i=='.':
+                ans+='[.]'
+            else:
+                ans+=i
+                
+        
+
+        return ans
