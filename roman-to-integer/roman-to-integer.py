d4a1f5bbffe6b09f3665266515808a5de405e994
Rafat Ashraf Joy <61883509+rajoy99@users.noreply.github.com>
2021-12-13 05:00:15 +0600

    Time: 48 ms (69.31%), Space: 14.2 MB (59.81%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,35 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        mapping= {
+            'I':1,
+            'IV':4,
+            'V':5,
+            'IX':9,
+            'X':10,
+            'XL':40,
+            'L':50,
+            'XC':90,
+            'C':100,
+            'CD':400,
+            'D':500,
+            'CM':900,
+            'M':1000
+            
+        }
+        
+        res=0
+        
+        i=0
+        while i<len(s):
+            
+            if s[i:i+2] in mapping:
+                res+=mapping[s[i:i+2]]
+                i+=2
+                
+            else:
+                res+=mapping[s[i]]
+                i+=1
+                
+        return res
+        
